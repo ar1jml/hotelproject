e20d6f45ded594d4be85f9bdd5745c3a48093076
Arafat <arafatjemal2015@gmail.com>
2026-07-10 13:53:06 +0300

    first commit

---
+++ frontend/src/components/header/Header.vue
@@ -0,0 +1,9 @@
+<script setup>
+import TopHeader from './TopHeader.vue'
+</script>
+
+<template>
+  <TopHeader />
+</template>
+
+<style scope> </style>
---
+++ frontend/src/components/header/TopHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>
+    <!-- Logo Area -->
+
+    <div>
+      <img src="@/assets/logo.png" alt="Logo" class="logo" />
+    </div>
+
+
+     <!-- Main Navigation -->
+
+     <nav>
+
+      <a>Home</a>
+      <a>Rooms</a>
+      <a>Restaurants</a>
+      <a>Blogs</a>
+      <a>Our Gallery</a>
+      <a>Contact Us</a>
+
+    </nav>
+  </header>
+</template>
+
+
+<script setup>
+
+</script>
