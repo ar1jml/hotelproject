8c306fa32d9345d5c4a75a87658db18a4bb560ba
Arafat <arafatjemal2015@gmail.com>
2026-07-10 15:43:37 +0300

    fixed logo

--- frontend/src/components/header/TopHeader.vue
+++ frontend/src/components/header/TopHeader.vue
@@ -2,7 +2,7 @@
   <header>
     <!-- Logo Area -->
 
-    <div>
+    <div class="logo-container">
       <img src="@/assets/logo.png" alt="Logo" class="logo" />
     </div>
 
@@ -10,18 +10,31 @@
      <!-- Main Navigation -->
 
      <nav>
-
       <a>Home</a>
       <a>Rooms</a>
       <a>Restaurants</a>
       <a>Blogs</a>
       <a>Our Gallery</a>
       <a>Contact Us</a>
-
     </nav>
+ <!-- Right Actions -->
   </header>
 </template>
 
+<style scoped>
+.logo {
+  width: 80px;
+  height: 80px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+.logo-container {
+  position: fixed;
+  left: 20px;
+  top: 20px;
+  z-index: 1000;
+}
+</style>
 
 <script setup>
 
